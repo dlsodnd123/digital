0946a93c517a63f4f4f17717a020f5b43a116147
lrw <dlsodnd123@naver.com>
2020-10-30 17:41:29 +0900

    JAVA : 상속

---
+++ java/src/day10/상속4.java
@@ -0,0 +1,27 @@
+package day10;
+//p.316 포함관계 설명 예제
+public class 상속4 {
+
+	public static void main(String[] args) {
+		/* 원은 도형이다  
+		 * - is a(상속)
+		 * - 도형 클래스가 부모, 원 클래스가 자식
+		 * 스마트폰은 카메라를 가지고 있다.
+		 * - has a 관계(포함)
+		 * - 카메라 클래스는 스마트폰 클래스의 멤버 변수이다.
+		 * */
+	}
+
+}
+class SmartPhone{
+	Camera camera; // 포함
+}
+class Camera{
+	
+}
+class shape{
+	
+}
+class Circle extends shape{ // 상속
+	
+}
