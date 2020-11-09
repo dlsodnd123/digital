e8b34191476f3d416d7faec9dc9fb520b03a4a14
lrw <dlsodnd123@naver.com>
2020-11-09 17:21:54 +0900

    복습

---
+++ java/src/day16/복습.java
@@ -0,0 +1,26 @@
+package day16;
+
+public class 복습 {
+	/* 복습
+	 * <컬렉션 프레임웍> :데이터를 효율적으로 관리하기 위한 것
+	 *  - Collection : 데이터 1개씩
+	 *   - List
+	 *    - 중복 가능
+	 *    - 순서 보장
+	 *   - set
+	 *    - 중복 불가능
+	 *    - 순서 보장 X
+	 *  - Map : 데이터 2개
+	 *   - key, value
+	 *   - key : 중복불가, value : 중복가능
+	 *  
+	 *  - Collection, List, Set, Map은 다 인터페이스 == 기능만, 객체를 만들 수 없다.
+	 *  - List의 구현 클래스들 : Vector, ArrayList, LinkedList 등
+	 *  
+	 *  - ArrayList : 배열, 검색이 좋고, 삽입/삭제가 나쁘다.
+	 *  - LinkedList : 검색이 나쁘고, 중간 삽입/삭제가 좋다.
+	 *
+	 * 지네릭스 : 메서드나 클래스를 만들 때 멤버 변수 또는 매개변수의 클래스가 고정된 클래스가 아닐 때 사용되는 것
+	 * */
+	
+}
