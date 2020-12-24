a860154d0cef8bc335ba7e570f258283a72d2720
lrw <dlsodnd123@naver.com>
2020-12-24 17:47:25 +0900

    내용 수정 및 추가

--- java/src/review2/Test11.java
+++ java/src/review2/Test11.java
@@ -16,6 +16,7 @@ public class Test11 {
 		 * 5. 종료
 		 * */
 		Scanner scan = new Scanner(System.in);
+		WordBook wb = new WordBook();
 		int menu = 0;
 		do {
 			System.out.println("------- 메뉴 ---------");
@@ -28,7 +29,7 @@ public class Test11 {
 			System.out.println("메뉴를 선택해주세요 : ");
 			menu = scan.nextInt();
 			switch(menu) {
-			case 1 : 
+			case 1 : wb.addWord(scan);
 			case 2 :
 			case 3 : 
 			case 4 :
@@ -45,45 +46,91 @@ public class Test11 {
 
 }
 class WordBook{
-	private String word;
-	private String mean;
-	
-	public String getWord() {
-		return word;
+	private ArrayList<Word> list = new ArrayList<Word>();
+
+	public ArrayList<Word> getList() {
+		return list;
 	}
-	public void setWord(String word) {
-		this.word = word;
-	}
-	public String getMean() {
-		return mean;
-	}
-	public void setMean(String mean) {
-		this.mean = mean;
+
+	public void setList(ArrayList<Word> list) {
+		this.list = list;
 	}
 	
 	public WordBook() {}
 	
-	public WordBook(String word, String mean) {
-		this.word = word;
-		this.mean = mean;		
+	public void addWord(Scanner scan){
+		System.out.print("단어 : ");
+		String word = scan.next();
+		System.out.print("의미 : ");
+		String mean = scan.next();
+		list.add(new Word(word,mean));
 	}
+
+}
+
+class Word implements Comparable{
+	private String word;
+	private String mean;
 	
+	public Word(String word, String mean) {
+		this.word = word;
+		this.mean = mean;
+	}
+
+	public String getWord() {
+		return word;
+	}
+
+	public void setWord(String word) {
+		this.word = word;
+	}
+
+	public String getMean() {
+		return mean;
+	}
+
+	public void setMean(String mean) {
+		this.mean = mean;
+	}
+
+	@Override
+	public int compareTo(Object o) {
+		Word word= (Word)o;
+		return this.word.compareTo(word.word);
+	}
+
 	@Override
 	public String toString() {
 		return "[" + word + " : " + mean + "]";
 	}
-	//단어 등록
-	public void wordAdd(Scanner scan) {
-		System.out.print("단어 입력 : ");
-		String word = scan.next();
-		System.out.print("의미 입력 : ");
-		String mean = scan.next();
-		
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((word == null) ? 0 : word.hashCode());
+		return result;
 	}
-	//단어 검색
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if(obj instanceof String) {
+			return word.equals(obj);
+		}
+		if (getClass() != obj.getClass())
+			return false;
+		Word other = (Word) obj;
+		if (word == null) {
+			if (other.word != null)
+				return false;
+		} else if (!word.equals(other.word))
+			return false;
+		return true;
+	}
 	
-	//단어 수정
-	
-	//단어 출력
 	
 }
